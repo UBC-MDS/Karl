6a38b7d7329b016a5b6af40d76dea883f9bcc507
Maud Boucherit <maud.boucherit@gmail.com>
2018-03-02 15:26:25 -0800

    Test for multiple linear regression with continuous features

--- tests/testthat/test_LinearRegression.R
+++ tests/testthat/test_LinearRegression.R
@@ -3,6 +3,9 @@
 #
 # This script test the methods from the LinearRegression.R script.
 
+## Packages
+library(matlib)
+
 ### Test simple linear regression for a continuous feature
 # Generate small data to test our function
 set.seed(4)
@@ -26,3 +29,25 @@ test_that("Testing LinearRegression for one continuous feature", {
   expect_equal(model$residuals, res)
 })
 
+
+### Test multi-linear regression for continuous features
+# Generate small data to test our function
+set.seed(4)
+X <- data.frame('ones' = rep.int(1, times = length(y)), 'X1' = rnorm(10), 
+                'X2' = rnorm(10), 'X3' = rnorm(10))
+y <- X$X1 + X$X2 + X$X3 + rnorm(10)
+
+# Fit a linear regression on the data
+model <- LinearRegression(X, y)
+
+# True values
+X_mat <- as.matrix(X)
+beta <- inv(t(X_mat)%*%X_mat)%*%t(X_mat)%*%y
+fit <- X_mat%*%beta
+res <- y - fit
+
+test_that("Testing LinearRegression for multi continuous features", {
+  expect_equal(model$weights, beta)
+  expect_equal(model$fitted, fit)
+  expect_equal(model$residuals, res)
+})
